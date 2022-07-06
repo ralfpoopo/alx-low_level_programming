96349c284d761dadb1f340e01fa8a4b3321a6a96
ralfpoopo <ralfpoopo@gmail.com>
2022-07-05 17:34:58 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main -check the code
+*
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+	int p;
+
+	for (p = 'a'; p <= 'z'; p++)
+	{
+		_putchar(p);
+	}
+		_putchar('\n');
