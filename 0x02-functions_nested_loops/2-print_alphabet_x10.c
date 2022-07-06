a83151e4322ed8281527ddfb8c8f3d1768f54dad
ralfpoopo <ralfpoopo@gmail.com>
2022-07-06 06:15:07 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; b <= 9; a++)
 	{
-		for (b = a; b <= z; b++)
+		for (b = 97; b <= 122; b++)
 		{
 			_putchar(b);
 		}
