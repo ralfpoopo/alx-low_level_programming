5ce27c83526955fc5194cdd24430b034e23cc390
ralfpoopo <ralfpoopo@gmail.com>
2022-07-06 17:15:04 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,8 +18,8 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-			       	else if (k < 10 && j != 0)
-				{	
+				else if (k < 10 && j != 0)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
@@ -33,7 +33,7 @@ void print_times_table(int n)
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-			       	else if (k >= 100)
+				else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
