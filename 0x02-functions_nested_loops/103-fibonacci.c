a799beba7fe2a96042520983eb8fe49de39cd3e4
ralfpoopo <ralfpoopo@gmail.com>
2022-07-06 17:44:57 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 	unsigned long int j, k, next, sum;
-	
+
 	j = 1;
-	
+
 	k = 2;
-						
+
 	sum = 0;
-	
+
 	for (i = 1; i <= 33; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
@@ -27,6 +27,6 @@ int main(void)
 		k = next;
 	}
 	printf("%lu\n", sum);
-	
+
 	return (0);
 }
