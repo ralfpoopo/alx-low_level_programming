4ce38890a28020a84b5cfefa7f75e70a60fc8a8e
ralfpoopo <ralfpoopo@gmail.com>
2022-08-02 09:44:05 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,8 +6,8 @@
 /**
  *  * _is_zero - determines if any number is zero
  *   * @argv: argument vector.
- *    *
- *     * Return: no return.
+ *
+ * Return: no return.
  */
 void _is_zero(char *argv[])
 {
@@ -38,8 +38,8 @@ void _is_zero(char *argv[])
  *  * _initialize_array - set memery to zero in a new array
  *   * @ar: char array.
  *    * @lar: length of the char array.
- *     *
- *      * Return: pointer of a char array.
+ *
+ * Return: pointer of a char array.
  */
 char *_initialize_array(char *ar, int lar)
 {
@@ -56,8 +56,8 @@ char *_initialize_array(char *ar, int lar)
  *   * and checks if number is in base 10.
  *    * @argv: arguments vector.
  *     * @n: row of the array.
- *      *
- *       * Return: length of the number.
+ *
+ * Return: length of the number.
  */
 int _checknum(char *argv[], int n)
 {
@@ -78,8 +78,8 @@ int _checknum(char *argv[], int n)
  *   * program that multiplies two positive numbers.
  *    * @argc: number of arguments.
  *     * @argv: arguments vector.
- *      *
- *       * Return: 0 - success.
+ *
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
